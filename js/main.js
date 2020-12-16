f1b0df0c6a0339063d8adf1aff661355c1231219
IlyaMBondarev <60775995+IlyaMBondarev@users.noreply.github.com>
2020-12-16 15:12:51 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ let width = document.documentElement.offsetWidth;
 svgs.forEach(svg => {
 
     let paths = svg.querySelectorAll('path');
+    let pathsWidth = paths[0].getBoundingClientRect().width;
+    let columns = Math.ceil(width/pathsWidth) / 2;
     let pathsX = [];
 
     for(let i = 0; i < paths.length; i++) {
@@ -21,7 +23,26 @@ svgs.forEach(svg => {
         return a.x - b.x;
     });
 
+    pathsX.forEach(pathX => {
+        for (let i = 0; i < columns; i++) {
+            if (pathX.x >= (i * columns) && pathX.x < ((i + 1) * columns)) {
+                pathX.turn = i;
+            }
+        }
+    })
+
     setInterval(() => {
+        for (let i = 0; i < pathsX.length; i++) {
+            setTimeout(() => {
+                paths[pathsX[i].id].style.fill = 'rgba(255,255,255,0.4)';
+            }, 50 * pathsX[i].turn)
+            setTimeout(() => {
+                paths[pathsX[i].id].style.fill = '';
+            }, 50 * pathsX[i].turn + 1000)
+        }
+    }, 20 * columns + 1000)
+
+    /*setInterval(() => {
         for (let i = 0; i < pathsX.length; i++) {
             setTimeout(() => {
                 paths[pathsX[i].id].style.fill = 'rgba(255,255,255,0.4)';
@@ -30,5 +51,5 @@ svgs.forEach(svg => {
                 paths[pathsX[i].id].style.fill = ''
             }, i + 1000)
         }
-    }, paths.length)
+    }, paths.length)*/
 });
