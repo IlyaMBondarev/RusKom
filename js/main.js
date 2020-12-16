a7b1e72955a3b96b83540549257d3d1843b05b26
IlyaMBondarev <60775995+IlyaMBondarev@users.noreply.github.com>
2020-12-16 14:52:50 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+document.querySelector('.wrapper').classList.add('loaded');
+
+// анимация svg под формой
+
+let svgs = document.querySelectorAll('.questions__back');
+let width = document.documentElement.offsetWidth;
+
+svgs.forEach(svg => {
+
+    let paths = svg.querySelectorAll('path');
+    let pathsX = [];
+
+    for(let i = 0; i < paths.length; i++) {
+        pathsX.push({})
+        pathsX[i].id = i;
+        pathsX[i].x = paths[i].getBoundingClientRect().x;
+    }
+
+    pathsX.sort(function(a,b) {
+        return a.x - b.x;
+    });
+
+    setInterval(() => {
+        for (let i = 0; i < pathsX.length; i++) {
+            setTimeout(() => {
+                paths[pathsX[i].id].style.fill = 'rgba(255,255,255,0.4)';
+            }, i)
+            setTimeout(() => {
+                paths[pathsX[i].id].style.fill = ''
+            }, i + 1000)
+        }
+    }, paths.length)
+});
