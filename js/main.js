f955cb10d926c9b446d3a4f8895723b571a68862
IlyaMBondarev <60775995+IlyaMBondarev@users.noreply.github.com>
2020-12-24 09:02:38 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -551,6 +551,25 @@ if ($('.faq').length) {
         })
     }
 }
+
+//замена img-svg на svg
+
+$('img.img-svg').each(function(){
+    let $img = $(this);
+    let imgClass = $img.attr('class');
+    let imgURL = $img.attr('src');
+    $.get(imgURL, function(data) {
+        let $svg = $(data).find('svg');
+        if(typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+        $svg = $svg.removeAttr('xmlns:a');
+        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+            $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+        }
+        $img.replaceWith($svg);
+    }, 'xml');
+});
 //карусель-слайдер на странице услуги
 
 $(document).ready(function() {
